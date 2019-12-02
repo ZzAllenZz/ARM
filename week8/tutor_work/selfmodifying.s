2049797d19055bf83a87463a98d78c2e4b213837
ZzAllenZz <44937834+ZzAllenZz@users.noreply.github.com>
2019-12-02 15:11:38 +0000

    ARM

---
+++ week8/tutor_work/selfmodifying.s
@@ -0,0 +1,18 @@
+	.section	.text
+	.align		2
+	.global		_start
+
+_start:
+	mov	r0, #42  
+	ldr	r1, =_start
+	ldr	r2, [r1]
+	mov	r3, r2
+	add	r2, r2, #0x01
+	mov	r4,  #0x0000fff
+	and	r2, r2, r4  @ mask out operand 2
+	orr	r3, r3, r2
+	str	r3, [r1]
+	b	_start
+@不断的改变第一条指令，second time： mov r0, #43，看mov 指令构成
+
+
